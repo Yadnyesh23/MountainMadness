6d9e80d43e939168aa9d13b14c844b2bd5d4e5ac
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-01-18 17:58:16 +0530

    Initialized a backend

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import errorHandler from "./middlewares/error.middleware.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Mountain Madness Backend Running 🚀");
+});
+
+app.use(errorHandler);
+
+export default app;
